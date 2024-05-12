4c7eb37ee88338df8c43f4980afd74e205eac026
steeltx <realsteel.gnu@gmail.com>
2024-05-11 23:52:42 -0600

    03 - Estilos iniciales de los botones

--- 03-iosCalculator/src/config/theme/app-theme.ts
+++ 03-iosCalculator/src/config/theme/app-theme.ts
@@ -31,5 +31,26 @@ export const styles = StyleSheet.create({
         fontSize: 40,
         textAlign: 'right',
         fontWeight: '300'
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginBottom: 18,
+        paddingHorizontal: 10
+    },
+    button: {
+        height: 80,
+        width: 80,
+        backgroundColor: colors.darkGray,
+        borderRadius: 100,
+        justifyContent: 'center',
+        marginHorizontal: 10
+    },
+    buttonText: {
+        textAlign: 'center',
+        padding: 10,
+        fontSize: 30,
+        color: 'white',
+        fontWeight: '300'
     }
 });
