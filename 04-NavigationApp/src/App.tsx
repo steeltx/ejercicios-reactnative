1324197d2d1ea4e3fd1a0d87cba9482bd3447586
steeltx <realsteel.gnu@gmail.com>
2024-06-09 23:46:41 -0600

    04 - Estructura inicial de carpetas y archivos

---
+++ 04-NavigationApp/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>App
+      </Text>
+    </View>
+  )
+}
---
+++ 04-NavigationApp/src/presentation/screens/tabs/Tab3Screen.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from 'react-native';
+
+export const Tab3Screen = () => {
+  return (
+    <View><Text>Tab3Screen</Text></View>
+  )
+}
